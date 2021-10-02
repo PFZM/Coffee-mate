2ac7c49037a571d8b7713991463658c13d874fa2
Anusha <anushaselvan@gmail.com>
2021-10-02 17:44:58 +1000

    Added Javascript code for form elements

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,4 +1,88 @@
-fetch(
+const welcomeBtnEl = document.querySelector("#welcome-btn");
+const aboutSectionEl = document.querySelector(".about-section");
+const formPreferencesEl = document.querySelector("#form-section");
+const formBtnEl = document.querySelector("#form-btn");
+const mainDisplayEl = document.querySelector("#main");
+const userNameEl = document.querySelector("#username");
+const dateOfBirthEl = document.querySelector("#DOB");
+const userLocationEl = document.querySelector("#location");
+const userMusicEl = document.querySelector("#user-music");
+
+
+// User enters and see a ewlcome header and the about section
+
+
+function getUserPreferences(){
+  return JSON.parse(localStorage.getItem("user")) || [];
+}
+
+function setUserPreferences(user){
+  localStorage.setItem("user", JSON.stringify(user));
+}
+
+function init(){
+  //Check if there is information in the localstorage if not display about section
+const user = getUserPreferences();
+if (user.length !== 0){
+displayMainSection();
+}
+return
+};
+
+
+
+//when user clicks the button display form appears and display section goes away
+function displayForm () {
+  aboutSectionEl.className = "hide";
+  formPreferencesEl.className = "form-container";
+
+};
+
+
+function displayMainSection (){
+  formPreferencesEl.className = "hide";
+  mainDisplayEl.className = "main-display";
+
+  console.log(userNameEl.value);
+
+     const user ={
+    name: userNameEl.value
+   // dob: dateOfBirthEl.value,
+   // location: userLocationEl.value,
+   // music: userMusic,
+  };
+
+  console.log(user);
+
+  
+  setUserPreferences (user);
+
+
+};
+
+
+userMusicEl.addEventListener("click", function (event){
+  event.preventDefault();
+const userSelection = event.target;
+
+if (userSelection.matches("button") === true){
+    const userMusic = userSelection.getAttribute("value");
+    console.log(userMusic);
+}
+})
+
+welcomeBtnEl.addEventListener("click", displayForm);
+formBtnEl.addEventListener("click", displayMainSection);
+
+
+//Form section: user fill the form and then information is storage in localstorage
+
+//then main display is shown
+
+
+
+
+/*fetch(
   "https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=-37.0201&lon=144.9646",
   {
     method: "GET",
@@ -13,4 +97,5 @@ fetch(
   })
   .catch((err) => {
     console.error(err);
-  });
+  });*/
+  init();
