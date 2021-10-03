a84bf68a1cf8da37260eba641de64cd383d9a2ea
PFZM <pfzm@hotmail.com>
2021-10-03 21:01:05 +1100

    fix issue with music event target

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,6 +1,7 @@
 const welcomeBtnEl = document.querySelector("#welcome-btn");
 const aboutSectionEl = document.querySelector(".about-section");
 const formPreferencesEl = document.querySelector("#form-section");
+const formFieldsEl = document.querySelector("#form");
 const formBtnEl = document.querySelector("#form-btn");
 const mainDisplayEl = document.querySelector("#main");
 const userNameEl = document.querySelector("#username");
@@ -11,11 +12,26 @@ const headerEl = document.querySelector("#header");
 const headerTitleEl = document.querySelector("#header-h1");
 const preferenceBtnEl = document.querySelector("#preferences");
 const displayTimeAndWeather = document.querySelector("#display-weather-time");
-
-// User enters and see a ewlcome header and the about section
+const cancelBtnEl = document.querySelector("#cancel-btn");
 
 welcomeBtnEl.addEventListener("click", displayForm);
 formBtnEl.addEventListener("click", getFormValues);
+preferenceBtnEl.addEventListener("click", displayForm);
+cancelBtnEl.addEventListener("click", function () {
+  const user = getUserPreferences();
+  displayMainSection(user);
+});
+
+const userFavGenres = [];
+userMusicEl.addEventListener("click", function (event) {
+  const userSelection = event.target;
+
+  if (userSelection.matches("button") === true) {
+    const userMusic = userSelection.getAttribute("value");
+    userFavGenres.push(userMusic);
+  }
+  console.log(userFavGenres);
+});
 
 function getUserPreferences() {
   return JSON.parse(localStorage.getItem("user")) || [];
@@ -34,32 +50,35 @@ function init() {
   return;
 }
 
-//when user clicks the button display form appears and display section goes away
+//when user clicks the button display form appears and about section goes away
 function displayForm() {
   aboutSectionEl.className = "hide";
   formPreferencesEl.className = "form-container";
+  mainDisplayEl.className = "hide";
+
+  const user = getUserPreferences();
+  if (user.length !== 0) {
+    cancelBtnEl.className = "btn";
+  } else {
+    cancelBtnEl.className = "hide";
+  }
 }
-
-// function musicOptions() {
-//   userMusicEl.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     const userSelection = event.target;
-
-//     if (userSelection.matches("button") === true) {
-//       const userMusic = userSelection.getAttribute("value");
-//       console.log(userMusic);
-//       return userMusic;
-//     }
-//   });
-// }
 
 function getFormValues() {
   const user = {
     name: userNameEl.value,
     dob: dateOfBirthEl.value,
     location: userLocationEl.value,
-    // music: musicOptions(),
+    music: userFavGenres,
   };
+
+  // validation that user needs to input all fields
+  if (!user.name && !user.dob && !user.location && !user.music) {
+    window.alert("Plaese fill in all the fields");
+    return;
+  }
+
+  localStorage.removeItem(user);
 
   setUserPreferences(user);
 
@@ -70,6 +89,7 @@ function displayMainSection(user) {
   aboutSectionEl.className = "hide";
   formPreferencesEl.className = "hide";
   mainDisplayEl.className = "main-display";
+  displayTimeAndWeather.innerHTML = "";
 
   // Header display
   headerTitleEl.textContent =
@@ -81,10 +101,10 @@ function displayMainSection(user) {
   currentDate.textContent = moment().format("dddd, DD-MMM-YYYY, hh:mm");
   displayTimeAndWeather.appendChild(currentDate);
 
+  // Retrieve weather and display
   retrieveWeather(user);
 
-  // const weather = data.main.temp;
-  console.log(user);
+  retrieveSongOfTheDay(user);
 }
 
 function retrieveWeather(user) {
@@ -113,28 +133,45 @@ function retrieveWeather(user) {
 }
 
 function displayWeather(data) {
+  displayTimeAndWeather.innerHTML = "";
+
+  const cityAndCountry = document.createElement("h2");
+  cityAndCountry.textContent = data.name + " - " + data.sys.country;
+  displayTimeAndWeather.appendChild(cityAndCountry);
+
+  const iconWeatherUrl =
+    "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+
+  const iconWeather = document.createElement("img");
+  iconWeather.src = iconWeatherUrl;
+  displayTimeAndWeather.appendChild(iconWeather);
+
   const temperature = document.createElement("p");
   temperature.textContent = "Temperature: " + data.main.temp + " °C";
   displayTimeAndWeather.appendChild(temperature);
 }
-//Form section: user fill the form and then information is storage in localstorage
 
-//then main display is shown
+function retrieveSongOfTheDay(user) {
+  const APIKeyYoutube = "AIzaSyBc58mT_-8rn6_TGyrZRhizEdMAXVqiRJQ";
 
-/*fetch(
-  "https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=-37.0201&lon=144.9646",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
-      "x-rapidapi-key": "18b41c1c7bmsheb630e6f2195bcfp1e66f6jsn0071376690aa",
-    },
-  }
-)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.error(err);
-  });*/
+  const MusicQueryURL =
+    "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&q=ranchera&chart=mostPopular&key=" +
+    APIKeyYoutube;
+
+  fetch(MusicQueryURL)
+    .then(function (response) {
+      if (!response.ok) {
+        alert("Error: " + response.statusText);
+        return;
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      alert("Unable to retrieve data");
+    });
+}
+
 init();
